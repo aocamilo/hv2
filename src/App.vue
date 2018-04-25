ed99ad495bbd65f9ffccf3b415c7fff4886362f1
Aocamilo <camilo_ar95@outlook.com>
2018-04-25 15:49:27 -0500

    hola

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template lang="pug">
   #app
-<<<<<<< HEAD
-    img(src='./images/pp.jpg' class="imagen")
-=======
-    img(src='https://aocamilo.github.io/Hv/dist/imagen.jpg' class="imagen")
->>>>>>> 0f423b68c208e43fc06b0a48c06f51850485b7de
+    img(src='https://aocamilo.github.io/Hv/dist/pp.jpg' class="imagen")
     br
     br
     h1 Perfil
